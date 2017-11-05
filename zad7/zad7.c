571acf1c61fec015cd9122041e8b4101a83150da
Kamil Przybysz <kprzybysz@linux-p110-14.inf.ug.edu.pl>
2017-11-05 13:08:59 +0100

    zad7

--- zad7/zad7.c
+++ zad7/zad7.c
@@ -2,16 +2,36 @@
 
 int main()
 {
-  int a=8;
-  int b=8;
+  int a;
+  int b;
+  printf("Podaj wysokość a:");
+  scanf("%d", &a);
+  printf("Podaj szerokość b:");
+  scanf("%d", &b);
 
-  for(int i=1; i<=b; i++)
+  if(a<0)
   {
-    printf("\n");
-    for(int i=1; i<=a; i++)
-    {
-      printf("*");
-    }
+    printf("Zła wysokość\n");
+    return 1;
   }
+  else if(b<0)
+    {
+      printf("Zła szerokość\n");
+      return 1;
+    }
+    else
+    {
+      for(int i=1; i<=a; i++)
+      {
+        putchar('\n');
+        for(int i=1; i<=b; i++)
+        {
+          putchar('*');
+        }
+
+      }
+        putchar('\n');
+    }
+
   return 0;
 }
