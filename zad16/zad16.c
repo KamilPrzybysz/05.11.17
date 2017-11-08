42cb271dbef0f31036dee664890415d2b283c533
Kamil Przybysz <kprzybysz@sigma-ng.inf.ug.edu.pl>
2017-11-08 19:12:58 +0100

    zad16

--- zad16/zad16.c
+++ zad16/zad16.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-	printf("Podaj liczbę kończącą przedział od 1: ");
+	printf("Podaj liczbę kończącą przedział od 0: ");
 	int n;
 	scanf("%d", &n);
 	while(n<0)
@@ -18,19 +18,25 @@ int main()
 	srand(time(NULL));
     	int liczba=(rand()%n);
      	int i;
- 	printf("Podaj liczbe w przedziale 1-%d\n", n);
+ 	printf("Podaj liczbe w przedziale od 0 do %d: ", n);
    	do
 	{
-        scanf("%d", &i);
+        	scanf("%d", &i);
+		while(i<0)
+		{
+			printf("Liczba musi być większa od 0!\n");
+                	printf("Podaj właściwą liczbę: ");
+                	scanf("%d", &i);
+        	}
 
-        if(i<liczba)
-
-        printf("Podałeś za małą liczbę!\n");
-
-        if(i>liczba)
-
-        printf("Podałeś za dużą liczbę!\n");
-
+        	if(i<liczba)
+		{
+        		printf("Podałeś za małą liczbę, spróbuj ponownie: ");
+		}
+        	if(i>liczba)
+		{
+       			printf("Podałeś za dużą liczbę, spróbuj ponownie: ");
+		}
     	}while(i!=liczba);
 
    	printf("Odgadłeś liczbę! Ta liczba to %d\n", liczba);
